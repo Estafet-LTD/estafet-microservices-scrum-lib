701d1dffd48e1fa4571a2d9b77242b9c94c4d38d
Dennis Williams <dennis.williams@estafet.com>
2018-05-13 21:34:08 +0100

    added logs

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -9,11 +9,17 @@ node {
 	}
 	
 	stage("increment version") {
+		println "read the file"
 		def pom = readFile('pom.xml');
+		println "get the snapshot version from pom"
 		snapshotVersion = new XmlSlurper().parseText(pom).version.text
+		println snapshotVersion
 		def matcher = snapshotVersion =~ /(d+\.d+\.)(d+)(\-SNAPSHOT)/
+		println "get the snapshot integral"
 		int snapshotIntegral = matcher[0][1]
+		println "get the nextSnapshotVersion"
 		nextSnapshotVersion = "${matcher[0][0]}${snapshotIntegral+1}-SNAPSHOT"
+		println "get the releaseVersion"
 		releaseVersion = "${matcher[0][0]}${snapshotIntegral}"
 		println nextSnapshotVersion
 		println releaseVersion
