5a254da0701151b5e4365b734bd219d08c29d617
Dennis Williams <dennis.williams@estafet.com>
2018-05-13 22:57:14 +0100

    got this running

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -11,19 +11,17 @@ node {
 	stage("increment version") {
 		println "read the file"
 		def pom = readFile('pom.xml');
-		//println pom
-		println "get the snapshot version from pom"
-		snapshotVersion = new XmlSlurper().parseText(pom).version.text
+		snapshotVersion = new XmlSlurper().parseText(pom).version
 		println snapshotVersion
 		def matcher = snapshotVersion =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
-		/* println "get the snapshot integral"
-		int snapshotIntegral = matcher[1]
+		println "get the snapshot integral"
+		def snapshotIntegral = matcher[0][1]
 		println "get the nextSnapshotVersion"
-		nextSnapshotVersion = "${matcher[0]${snapshotIntegral+1}-SNAPSHOT"
+		nextSnapshotVersion = "${matcher[0][1]}${snapshotIntegral}-SNAPSHOT"
 		println "get the releaseVersion"
-		releaseVersion = "${matcher[0]${snapshotIntegral}"
+		releaseVersion = "${matcher[0][1]}${snapshotIntegral}"
 		println nextSnapshotVersion
-		println releaseVersion */
+		println releaseVersion
 	}
 	
 }
