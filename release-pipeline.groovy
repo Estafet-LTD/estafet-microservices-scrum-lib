e414f41f31efb1bf014bd991ca11f1920f1c9f50
Dennis Williams <dennis.williams@estafet.com>
2018-05-13 18:28:26 +0100

    make the snapshots unique

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -1,7 +1,14 @@
 node {
 
+	def currentVersion;
+	def nextVersion;
+
 	stage("checkout") {
 		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-lib"
+	}
+	
+	stage("increment version") {
+		
 	}
 	
 }
