5ec54e843434bf9997f4a008627b48b068f4003d
Dennis Williams <dennis.williams@estafet.com>
2018-05-13 21:15:34 +0100

    calculate release versioons

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -1,14 +1,22 @@
 node {
 
-	def currentVersion;
-	def nextVersion;
+	def snapshotVersion;
+	def nextSnapshotVersion;
+	def releaseVersion
 
 	stage("checkout") {
 		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-lib"
 	}
 	
 	stage("increment version") {
-		
+		def pom = readFile('pom.xml');
+		snapshotVersion = new XmlSlurper().parseText(json).version.text
+		def matcher = snapshotVersion =~ /(d+\.d+\.)(d+)(\-SNAPSHOT)/
+		int snapshotIntegral = matcher[0][2]
+		nextSnapshotVersion = "${matcher[0][1]}${snapshotIntegral+1}-SNAPSHOT"
+		releaseVersion = "${matcher[0][1]}${snapshotIntegral}"
+		println nextSnapshotVersion
+		println releaseVersion
 	}
 	
 }
