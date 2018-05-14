a59da2f1f5c4ce9948eaaa4f87b30c6c97215a84
Dennis Williams <dennis.williams@estafet.com>
2018-05-14 13:00:38 +0100

    ok push the changes

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -30,7 +30,8 @@ node('maven') {
         sh "git config --global user.email \"jenkins@estafet.com\""
         sh "git config --global user.name \"jenkins\""
         withMaven(mavenSettingsConfig: 'microservices-scrum') {
-			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=true -DlocalCheckout=false -DpreparationGoals=initialize -B"
+			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=false -DpreparationGoals=initialize -B"
+			sh "git push https://${username()}:${password()}@github.com/Estafet-LTD/estafet-microservices-scrum-lib"
 		} 
 	}	
 	
