e1f77d98377e78e052b7e5001c3ea7b00de87444
Dennis Williams <dennis.williams@estafet.com>
2018-05-21 18:59:43 +0100

    updated method for reading config files from jars

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -6,12 +6,6 @@ node('maven') {
 		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-lib"
 	}
 
-	stage("unit tests") {
-		withMaven(mavenSettingsConfig: 'microservices-scrum') {
-	      sh "mvn clean test"
-	    } 
-	}
-
 	stage("deploy snapshots") {
 		withMaven(mavenSettingsConfig: 'microservices-scrum') {
  			sh "mvn clean deploy -Dmaven.test.skip=true"
--- estafet-microservices-scrum-lib-data/src/main/java/com/estafet/microservices/scrum/lib/data/cleanser/DataCleanser.java
+++ estafet-microservices-scrum-lib-data/src/main/java/com/estafet/microservices/scrum/lib/data/cleanser/DataCleanser.java
@@ -1,6 +1,8 @@
 package com.estafet.microservices.scrum.lib.data.cleanser;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.google.common.io.Resources;
 import com.thoughtworks.xstream.XStream;
@@ -8,13 +10,26 @@ import com.thoughtworks.xstream.XStream;
 public class DataCleanser {
 
 	public static void clean() {
-		File file = new File(Resources.getResource("services.xml").getPath());
-		XStream xStream = new XStream();
-		xStream.processAnnotations(Services.class);
-		Services services = (Services)xStream.fromXML(file);
-		for (Service service : services.getServices()) {
-			service.clean();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(Resources.getResource("services.xml").openStream()));
+			XStream xStream = new XStream();
+			xStream.processAnnotations(Services.class);
+			Services services = (Services) xStream.fromXML(reader);
+			for (Service service : services.getServices()) {
+				service.clean();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
 		}
 	}
-	
+
 }
